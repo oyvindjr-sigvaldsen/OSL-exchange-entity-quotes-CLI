dda9177f27b979ca06374d2911ae6ef290719b14
oyvindjr-sigvaldsen <oyvindjr.sigvaldsen@me.com>
2020-02-18 11:28:30 +0100

    Almost finished retrieve_ticker_prices.py script

--- retrieve_ticker_prices.py
+++ retrieve_ticker_prices.py
@@ -3,6 +3,7 @@
 # imports
 import requests, bs4, time
 from datetime import datetime as dt
+from tqdm import tqdm
 
 import os, sys
 absolute_path  = "/Users/o/Sites/github-repositories/OSL-exchange-live-watchlist/local-modules/"
@@ -14,20 +15,34 @@ def main():
 
 	entity_names, complete_URL = rsd.main()
 
-	for entity in complete_URL:
+	entity_quotes = []
 
-		request = requests.get(entity)
+	for i in tqdm(range(140, len(entity_names))):
+
+		request = requests.get(complete_URL[i])
 		soup = bs4.BeautifulSoup(request.text, "html.parser")
 
-		buy_price_class = "Typography__Span-sc-10mju41-0 htqjlq Typography__StyledTypography-sc-10mju41-1 famDvB StatsBox__StyledPriceText-sc-1p4v3dm-2 emJMXq"
+		quotes_wrapper_class = "Typography__Span-sc-10mju41-0 htqjlq Typography__StyledTypography-sc-10mju41-1 famDvB StatsBox__StyledPriceText-sc-1p4v3dm-2 emJMXq"
+		quotes_wrapper = soup.find_all("span", {"class" : quotes_wrapper_class})
 
-		buy_price = soup.find_all("span", {"class" : buy_price_class})
+		buy_price, sell_price = quotes_wrapper[0].text, quotes_wrapper[1].text
+		high, low = quotes_wrapper[2].text, quotes_wrapper[3].text
+		volume = quotes_wrapper[4].text
 
-		for i in buy_price:
-			print(i.text)
-		print(buy_price)
 		print("\n")
+		print(entity_names[i])
 
+		entity_price_info = [
+								float(buy_price.replace(",", ".")),
+								float(sell_price.replace(",", ".")),
+								float(high.replace(",", ".")),
+								float(low.replace(",", ".")),
+								volume
+							]
+
+		entity_quotes.append(entity_price_info)
+		print(entity_price_info)
+		print("\n")
 
 		#live_time_element = soup.find_all("time")[0].text
 		#live_time = dt.strptime(str(live_time_element), "%H:%M:%S")
